96a94c4c51f3d50b5e381e9e926b35fc5cccc2c0
Gagnar1999 <Gagannarang1999@gmail.com>
2021-04-20 11:25:09 +0530

    added 1 py file

---
+++ Guess_Game.py
@@ -0,0 +1,29 @@
+from random import randint
+while input("DO YOU WANT TO PLAY VERY INTERESTING GUESS GAME!!(YES/NO)").strip().lower() == 'yes':
+    com_guess = randint(1,50)
+    chance = 5
+    while chance > 0:  
+        print(f"\n\n You Have {chance} Left!!")
+        user_guess = int(input("Please Enter your Guess"))
+        if user_guess > 50 or user_guess < 1:
+            print("\n\n!!!!WARNING!!! Please Enter (1-50)only")
+            continue
+        if user_guess == com_guess:
+            print("\n\n!!!YOU ARE SUCH A MASTER!!!!")
+            break
+        elif user_guess > com_guess:
+            print("\n\n!!HINT: Please think some low value!!")
+        else:
+            print("\n\n!!HINT: Please think some High value!!")
+        chance -=1    
+    else:
+        print(f"\n\n!!Computer Guess was {com_guess}!!")
+        print("\n\n!!ha ha ha!! You such a Looser!!!")
+        print("\n\n")             
+                 
+                            
+             
+                       
+             
+             
+ 
